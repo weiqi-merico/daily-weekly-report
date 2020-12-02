c32b15eb1dd3da007cc40917a56ff5945b3058e5
wei.qi <wei.qi@meri.co>
2020-12-02 20:42:22 +0800

    Create 20201202_6.java

---
+++ 20201202_6.java
@@ -0,0 +1,23 @@
+package com.merico.selenium.common;
+
+/**
+ * Global constants
+ *
+ */
+public class Constants {
+	/**
+	 * The variable filename for the project
+	 */
+	public static final String SELENIUM_CONFIG = "selenium-vars.properties";
+	
+	/**
+	 * The file name of page to URL
+	 */
+	public static final String PAGE_URL_MAPPING_FILE = "page-url.properties";
+	
+    /**
+     * The key of WebDriver in TestContext Attributes
+     */
+	public static final String CONTEXT_KEY_DRIVER = "CONTEXT_KEY_DRIVER";
+
+}
